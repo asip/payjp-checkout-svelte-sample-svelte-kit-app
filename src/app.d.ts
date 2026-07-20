43bcf34d2b47b78fc1c6c6ec10709da4138a86bb
Yasumasa Ashida <ys.ashida@gmail.com>
2026-07-20 23:01:37 +0900

    chore: Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,5 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 
-import type {
-  PayjpCheckoutResponse,
-  PayjpCheckoutErrorResponse,
-} from './components/payjp-checkout.svelte'
-
 // for information about these interfaces
 declare global {
   namespace App {
